b7a8a1d72ba5d452813ce0ed80ad225d7306beed
Milena Kari <milenakari@yahoo.com>
2019-10-18 20:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 
 append.push("Odie");
-append.shift()
+prepend.shift("Odie");
+removeLast.pop();
+removefirst.unshift();
